8f33b4932df369e5ae0435ef050d1c8ec3513382
Keifer.Gu <keifergu@gmail.com>
2017-03-08 21:34:26 +0800

    初步完成数据更新与读取

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,43 +1,80 @@
 <template>
     <div v-show="visibility" class="comment">
         <div>{{text}}</div>
-        <input type="text" name="newComment">
+        <div v-for="item in comments">
+          {{item.comment}}
+        </div>
+        <input v-model="newComment" @keyup.enter="addComment">
         <input type="submit" name="submit" @click="addComment">
     </div>
 </template>
 
 <script>
-import AV from "leancloud-storage";
+import AV from 'leancloud-storage';
+
 const appId = '2y7v8BdJxedXm5a5sNBji5Aj-gzGzoHsz';
 const appKey = 'heXMkHNlopCuUXWzYPpnjcRw';
 AV.init({ appId, appKey });
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
+
+let Comment = AV.Object.extend('Comments');
+
 export default {
   name: 'comment',
   data() {
     return {
       visibility: false,
-      text: "",
+      text: '',
+      newComment: '',
       comments: []
     };
   },
+  watch: {
+    text: function(){
+      this.comments = [];
+      let queryUrl = new AV.Query('Comments');
+      let queryText = new AV.Query('Comments');
+      queryUrl.equalTo('url', window.location.href);
+      queryText.equalTo('text', this.text);
+      let query = AV.Query.and(queryUrl, queryText);
+      query.find().then((data) => {
+        console.log(data);
+        data.forEach((value) => {
+          this.comments.push(value.attributes);
+        })
+      });
+    }
+  },
   mounted() {
-    this.$on("visibility",(status) => {
+    this.$on('visibility', (status) => {
       this.visibility = status;
     });
-    this.$on("text",(text) => {
+    this.$on('text', (text) => {
       this.text = text;
     });
   },
   methods: {
     addComment(){
+      let _newcomment = this.newComment;
+      // 乐观更新
+      this.comments.push({comment:_newcomment});
+      // 构建数据储存对象
+      let comment = new Comment();
+      comment.set('url', window.location.href);
+      comment.set('text', this.text);
+      comment.set('comment', _newcomment);
 
+      // 清空输入框
+      this.newComment = '';
+
+      // 保存数据
+      comment.save().then((data) => {
+        console.log(data.id)
+      }, (error)=>{
+        // 保存失败时删除最后的一个comment
+        this.comments.pop();
+        this.newComment = _newcomment;
+        console.error('false'+error.message);
+      })
     }
   }
 }
